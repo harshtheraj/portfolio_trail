65901b30f63d634025bcd649289aeffff6d091e5
harshtheraj <harshtheraj@gmail.com>
2025-08-07 00:35:37 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+
+import NavBar from "./components/NavBar";
+import Experience from "./sections/Experience";
+import FeatureCards from "./sections/FeatureCards";
+import Hero from "./sections/Hero";
+import ShowcaseSection from "./sections/ShowcaseSection";
+import LogoShowcase from "./sections/LogoShowcase";
+import TechStack from "./sections/TechStack";
+import Testimonials from "./sections/Testimonials";
+import Contact from "./sections/Contact";
+import Footer from "./sections/Footer";
+
+const App = () => {
+  return (
+    <>
+        <NavBar/>
+        <Hero/>
+        <ShowcaseSection/>
+        <LogoShowcase />
+        <FeatureCards/>
+        <Experience/>
+        <TechStack/>
+        <Testimonials/>
+        <Contact/>
+        <Footer/>
+    </>
+  );
+};
+
+export default App;
